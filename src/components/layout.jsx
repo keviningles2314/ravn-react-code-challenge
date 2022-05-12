0f7413911395924bd480c2201310396e97b8a67b
kevin_ingles96 <kfabian@applaudostudios.dev>
2022-05-12 01:48:23 -0600

    💄 Update Home UI

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -2,7 +2,7 @@ import Sidebar from './Sidebar';
 
 const Layout = ({ children }) => {
   return (
-    <div className="p-8 flex bg-[#212529] h-screen text-white gap-5">
+    <div className="p-8 flex flex-grow bg-[#212529] h-screen text-white gap-5 overflow-hidden w-full">
       <Sidebar />
       <main className="w-full">{children}</main>
     </div>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,13 +1,21 @@
+import ColumnItem from '../components/ColumnItem';
 import Layout from '../components/layout';
 
 const Home = () => {
+  const statusArray = ['BACKLOG', 'CANCELLED', 'DONE', 'IN_PROGRESS', 'TODO'];
   return (
     <Layout>
-      <div className="flex flex-col">
-        <div className="w-full flex justify-end">
-          <div className="bg-[#DA584B] w-8 h-8 rounded-lg flex items-center justify-center text-lg">
+      <div className="flex flex-col box-border flex-grow h-screen">
+        <div className="w-full flex justify-end box-border">
+          <div className="bg-[#DA584B] w-8 h-8 rounded-lg box-border flex items-center justify-center text-lg">
             <h1>+</h1>
           </div>
+        </div>
+
+        <div className="columns flex gap-5 overflow-x-scroll overflow-y-scroll w-11/12 box-border">
+          {statusArray.map((item) => {
+            return <ColumnItem title={item} />;
+          })}
         </div>
       </div>
     </Layout>
