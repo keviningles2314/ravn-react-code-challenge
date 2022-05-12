5405e9073beaad8a80c00a3dcd0c38bd23d64f04
kevin_ingles96 <kfabian@applaudostudios.dev>
2022-05-11 19:45:51 -0600

    💄 Add dashboard menu item to sidebar

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -2,7 +2,7 @@ import Sidebar from './Sidebar';
 
 const Layout = ({ children }) => {
   return (
-    <div className="p-8 flex">
+    <div className="p-8 flex bg-[#212529] h-screen text-white">
       <Sidebar />
       <main>{children}</main>
     </div>
