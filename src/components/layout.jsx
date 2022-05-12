335ad70216ff062bc67b39aa84eb9ac3eccaf927
kevin_ingles96 <kfabian@applaudostudios.dev>
2022-05-11 21:09:21 -0600

    💄 Add plus icon

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -2,9 +2,9 @@ import Sidebar from './Sidebar';
 
 const Layout = ({ children }) => {
   return (
-    <div className="p-8 flex bg-[#212529] h-screen text-white">
+    <div className="p-8 flex bg-[#212529] h-screen text-white gap-5">
       <Sidebar />
-      <main>{children}</main>
+      <main className="w-full">{children}</main>
     </div>
   );
 };
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,7 +3,13 @@ import Layout from '../components/layout';
 const Home = () => {
   return (
     <Layout>
-      <h1 className="font-bold underline">Home</h1>
+      <div className="flex flex-col">
+        <div className="w-full flex justify-end">
+          <div className="bg-[#DA584B] w-8 h-8 rounded-lg flex items-center justify-center text-lg">
+            <h1>+</h1>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
