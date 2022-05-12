5d842afc8ac82444933e7db242fa13252adf09f8
kevin_ingles96 <kfabian@applaudostudios.dev>
2022-05-11 18:58:51 -0600

    💄 Update layout style

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -2,10 +2,10 @@ import Sidebar from './Sidebar';
 
 const Layout = ({ children }) => {
   return (
-    <>
+    <div className="p-8 flex">
       <Sidebar />
       <main>{children}</main>
-    </>
+    </div>
   );
 };
 export default Layout;
